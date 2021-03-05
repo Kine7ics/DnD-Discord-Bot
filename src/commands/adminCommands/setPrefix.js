c9cef1f842049fb5285373ed369c1dd8269df2c9
Kine7ics <Kinetics@github.com>
2021-03-05 16:33:08 +0100

    changed bot to use config.json for settings

--- src/commands/adminCommands/setPrefix.js
+++ src/commands/adminCommands/setPrefix.js
@@ -5,29 +5,11 @@ module.exports = {
   minArgs: 1,
   maxArgs: 1,
   callback: async (message, arguments) => {
-    const mongo = require("../../mongo"),
-      settingSchema = require("../../schemas/settings-schema");
+    const config = require("../../config.json");
 
-    const { guild } = message;
-    const newPrefix = arguments[0];
+    config.prefix = arguments[0];
 
-    await mongo().then(async (mongoose) => {
-      try {
-        await settingSchema
-          .findOneAndUpdate(
-            { _id: guild.id },
-            { _id: guild.id, commandPrefix: newPrefix },
-            { upsert: true }
-          )
-          .then(() => {
-            message.reply(
-              `the command prefix has been changed to: "${newPrefix}"`
-            );
-          });
-      } finally {
-        await mongoose.connection.close();
-      }
-    });
+    message.reply(`the command prefix has been changed to: "${newPrefix}"`);
   },
   permissions: ["ADMINISTRATOR"],
   description: "changes the command prefix.",
--- src/commands/command-base.js
+++ src/commands/command-base.js
@@ -1,152 +1,133 @@
 const validatePermissions = (permissions) => {
-    const validPermissions = [
-        "CREATE_INSTANT_INVITE",
-        "KICK_MEMBERS",
-        "BAN_MEMBERS",
-        "ADMINISTRATOR",
-        "MANAGE_CHANNELS",
-        "MANAGE_GUILD",
-        "ADD_REACTIONS",
-        "VIEW_AUDIT_LOG",
-        "PRIORITY_SPEAKER",
-        "STREAM",
-        "VIEW_CHANNEL",
-        "SEND_MESSAGES",
-        "SEND_TTS_MESSAGES",
-        "MANAGE_MESSAGES",
-        "EMBED_LINKS",
-        "ATTACH_FILES",
-        "READ_MESSAGE_HISTORY",
-        "MENTION_EVERYONE",
-        "USE_EXTERNAL_EMOJIS",
-        "VIEW_GUILD_INSIGHTS",
-        "CONNECT",
-        "SPEAK",
-        "MUTE_MEMBERS",
-        "DEAFEN_MEMBERS",
-        "MOVE_MEMBERS",
-        "USE_VAD",
-        "CHANGE_NICKNAME",
-        "MANAGE_NICKNAMES",
-        "MANAGE_ROLES",
-        "MANAGE_WEBHOOKS",
-        "MANAGE_EMOJIS",
-    ];
+  const validPermissions = [
+    "CREATE_INSTANT_INVITE",
+    "KICK_MEMBERS",
+    "BAN_MEMBERS",
+    "ADMINISTRATOR",
+    "MANAGE_CHANNELS",
+    "MANAGE_GUILD",
+    "ADD_REACTIONS",
+    "VIEW_AUDIT_LOG",
+    "PRIORITY_SPEAKER",
+    "STREAM",
+    "VIEW_CHANNEL",
+    "SEND_MESSAGES",
+    "SEND_TTS_MESSAGES",
+    "MANAGE_MESSAGES",
+    "EMBED_LINKS",
+    "ATTACH_FILES",
+    "READ_MESSAGE_HISTORY",
+    "MENTION_EVERYONE",
+    "USE_EXTERNAL_EMOJIS",
+    "VIEW_GUILD_INSIGHTS",
+    "CONNECT",
+    "SPEAK",
+    "MUTE_MEMBERS",
+    "DEAFEN_MEMBERS",
+    "MOVE_MEMBERS",
+    "USE_VAD",
+    "CHANGE_NICKNAME",
+    "MANAGE_NICKNAMES",
+    "MANAGE_ROLES",
+    "MANAGE_WEBHOOKS",
+    "MANAGE_EMOJIS",
+  ];
 
-    for (const permission of permissions) {
-        if (!validPermissions.includes(permission)) {
-            throw new Error(`Unknown permission node "${permission}"`);
-        }
+  for (const permission of permissions) {
+    if (!validPermissions.includes(permission)) {
+      throw new Error(`Unknown permission node "${permission}"`);
     }
+  }
 };
 
-const getPrefix = async (guild) => {
-    const mongo = require("../mongo");
-    const settingsSchema = require("../schemas/settings-schema");
-
-    let result = "!";
-
-    await mongo().then(async (mongoose) => {
-        try {
-            await settingsSchema.findOne({ _id: guild.id }).then((settings) => {
-                result = settings.commandPrefix;
-            });
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoose.connection.close();
-        }
-    });
-
-    return result;
-};
+const config = require("../config.json");
 
 module.exports = (client, commandOptions) => {
-    let {
-        commands,
-        expectedArgs = "",
-        permissionError = "you do not have permission to run this command",
-        minArgs = 0,
-        maxArgs = null,
-        permissions = [],
-        requiredRoles = [],
-        callback,
-    } = commandOptions;
+  let {
+    commands,
+    expectedArgs = "",
+    permissionError = "you do not have permission to run this command",
+    minArgs = 0,
+    maxArgs = null,
+    permissions = [],
+    requiredRoles = [],
+    callback,
+  } = commandOptions;
 
-    // Ensure the commands parameter is in the form of an array.
-    if (typeof commands === "string") {
-        commands = [commands];
+  // Ensure the commands parameter is in the form of an array.
+  if (typeof commands === "string") {
+    commands = [commands];
+  }
+
+  // Ensure the permissions are in an array and valid.
+  if (permissions.length) {
+    if (typeof permissions === "string") {
+      permissions = [permissions];
     }
 
-    // Ensure the permissions are in an array and valid.
-    if (permissions.length) {
-        if (typeof permissions === "string") {
-            permissions = [permissions];
+    validatePermissions(permissions);
+  }
+
+  client.on("message", (message) => {
+    const { member, content, guild } = message;
+
+    for (const alias of commands) {
+      if (
+        content
+          .toLowerCase()
+          .startsWith(`${config.prefix}${alias.toLowerCase()}`)
+      ) {
+        // A command has been ran
+
+        // Ensure the user has the required permission(s)
+        for (const permission of permissions) {
+          if (!member.hasPermission(permission)) {
+            message.reply(permissionError);
+            return;
+          }
         }
 
-        validatePermissions(permissions);
+        // Ensure the user has the required role(s)
+        for (const requiredRole of requiredRoles) {
+          const role = guild.roles.cache.find(
+            (role) => role.name === requiredRole
+          );
+
+          if (!role || !member.roles.cache.has(role.id)) {
+            message.reply(
+              `You must have the "${requiredRole}" to use this command.`
+            );
+            return;
+          }
+        }
+
+        const arguments = content.split(/[ ]+/);
+
+        // Remove the first index as this is the command.
+        arguments.shift();
+
+        // Ensure we have the correct number of arguments
+        if (
+          arguments.length < minArgs ||
+          (maxArgs !== null && arguments.length > maxArgs)
+        ) {
+          message.reply(
+            `Incorrect syntax! Use ${config.prefix}${alias} ${expectedArgs}`
+          );
+          return;
+        }
+
+        // Handle the custom command code.
+        callback(message, arguments, arguments.join(" "));
+        return;
+      }
     }
+  });
 
-    client.on("message", async (message) => {
-        const { member, content, guild } = message;
-
-        await getPrefix(guild).then((prefix) => {
-            for (const alias of commands) {
-                if (
-                    content.toLowerCase().startsWith(`${prefix}${alias.toLowerCase()}`)
-                ) {
-                    // A command has been ran
-
-                    // Ensure the user has the required permission(s)
-                    for (const permission of permissions) {
-                        if (!member.hasPermission(permission)) {
-                            message.reply(permissionError);
-                            return;
-                        }
-                    }
-
-                    // Ensure the user has the required role(s)
-                    for (const requiredRole of requiredRoles) {
-                        const role = guild.roles.cache.find(
-                            (role) => role.name === requiredRole
-                        );
-
-                        if (!role || !member.roles.cache.has(role.id)) {
-                            message.reply(
-                                `You must have the "${requiredRole}" to use this command.`
-                            );
-                            return;
-                        }
-                    }
-
-                    const arguments = content.split(/[ ]+/);
-
-                    // Remove the first index as this is the command.
-                    arguments.shift();
-
-                    // Ensure we have the correct number of arguments
-                    if (
-                        arguments.length < minArgs ||
-                        (maxArgs !== null && arguments.length > maxArgs)
-                    ) {
-                        message.reply(
-                            `Incorrect syntax! Use ${prefix}${alias} ${expectedArgs}`
-                        );
-                        return;
-                    }
-
-                    // Handle the custom command code.
-                    callback(message, arguments, arguments.join(" "));
-                    return;
-                }
-            }
-        });
-    });
-
-    return {
-        requiredRoles: requiredRoles,
-        commands: commands,
-        requiredPermissions: permissions,
-        expectedArgs: expectedArgs,
-    };
+  return {
+    requiredRoles: requiredRoles,
+    commands: commands,
+    requiredPermissions: permissions,
+    expectedArgs: expectedArgs,
+  };
 };
